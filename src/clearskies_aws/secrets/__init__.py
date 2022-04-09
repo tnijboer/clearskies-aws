3430df95de470b8b381faeeb20b8ca0f01a11b92
Conor Mancone <cmancone@gmail.com>
2022-04-09 16:16:38 -0400

    Misc updates, version bump

--- src/clearskies_aws/secrets/__init__.py
+++ src/clearskies_aws/secrets/__init__.py
@@ -1,2 +1,3 @@
 from .parameter_store import ParameterStore
 from .secrets_manager import SecretsManager
+from . import additional_configs
---
+++ src/clearskies_aws/secrets/additional_configs/__init__.py
@@ -0,0 +1,25 @@
+from .mysql_connection_dynamic_producer_via_ssh_cert_bastion_additional_config import MySQLConnectionDynamicProducerViaSSHCertBastionAdditionalConfig
+def mysql_connection_dynamic_producer_via_ssh_cert_bastion(
+    producer_name=None,
+    bastion_host=None,
+    bastion_name=None,
+    bastion_region=None,
+    bastion_username=None,
+    public_key_file_path=None,
+    cert_issuer_name=None,
+    database_host=None,
+    database_name=None,
+    local_proxy_port=None
+):
+    return MySQLConnectionDynamicProducerViaSSHCertBastionAdditionalConfig(
+        producer_name=producer_name,
+        bastion_host=bastion_host,
+        bastion_name=bastion_name,
+        bastion_region=bastion_region,
+        bastion_username=bastion_username,
+        cert_issuer_name=cert_issuer_name,
+        public_key_file_path=public_key_file_path,
+        database_host=database_host,
+        database_name=database_name,
+        local_proxy_port=local_proxy_port,
+    )
