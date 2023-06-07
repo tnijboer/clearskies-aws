dd8205af9ecb0672c37ba1591338f966bd675089
Tom Nijboer <tom.nijboer@cimpress.com>
2023-06-07 17:06:41 +0200

    sns event

---
+++ src/clearskies_aws/contexts/lambda_sns_event.py
@@ -0,0 +1,46 @@
+from ..input_outputs import LambdaSnsEvent as LambdaSnsEventInputOutput
+from ..di import StandardDependencies
+from clearskies.contexts.build_context import build_context
+from clearskies.contexts.context import Context
+from clearskies.authentication import public
+
+class LambdaSnsEvent(Context):
+    def __init__(self, di):
+        super().__init__(di)
+
+    def finalize_handler_config(self, config):
+        return {
+            'authentication': public(),
+            **config,
+        }
+
+    def __call__(self, event, context):
+        if self.handler is None:
+            raise ValueError("Cannot execute LambdaSnsEvent context without first configuring it")
+
+        for record in event['Records'][0]['Sns']:
+            try:
+                self.handler(LambdaSnsEventInputOutput(record['Message'], context))
+            except Exception as e:
+                print('Failed message ' + record['MessageId'] + ' being returned for retry.  Error error: ' + str(e))
+                item_failures.append({'itemIdentifier': record['MessageId']})
+
+        return {}
+
+def lambda_sns_event(
+    application,
+    di_class=StandardDependencies,
+    bindings=None,
+    binding_classes=None,
+    binding_modules=None,
+    additional_configs=None,
+):
+    return build_context(
+        LambdaSnsEvent,
+        application,
+        di_class=di_class,
+        bindings=bindings,
+        binding_classes=binding_classes,
+        binding_modules=binding_modules,
+        additional_configs=additional_configs,
+    )
