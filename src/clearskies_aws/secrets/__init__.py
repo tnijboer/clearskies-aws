49d38183490ed5f8ace604846ca6794808a7ae6e
Conor Mancone <cmancone@gmail.com>
2023-06-20 06:25:32 -0400

    sigh

--- src/clearskies_aws/secrets/__init__.py
+++ src/clearskies_aws/secrets/__init__.py
@@ -2,5 +2,5 @@ from .parameter_store import ParameterStore
 from .secrets_manager import SecretsManager
 from .akeyless_with_ssm_cache import AkeylessWithSsmCache
 from . import additional_configs
-def akeyless_with_cache(*args, **kwargs):
-    return BindingConfig(AkeylessWithCache, *args, **kwargs)
+def akeyless_with_ssm_cache(*args, **kwargs):
+    return BindingConfig(AkeylessWithSsmCache, *args, **kwargs)
