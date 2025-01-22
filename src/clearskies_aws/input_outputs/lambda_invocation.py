260d0a5db1d569f24e548477a107d112e02d3edf
Conor Mancone <cmancone@gmail.com>
2025-01-22 08:40:13 -0500

    Fix: add non-json-bodies flag to lambda invocation

--- src/clearskies_aws/input_outputs/lambda_invocation.py
+++ src/clearskies_aws/input_outputs/lambda_invocation.py
@@ -23,7 +23,9 @@ class LambdaInvocation(LambdaAPIGateway):
     def get_body(self):
         return self._event
 
-    def json_body(self, required=True):
+    def json_body(self, required=True, allow_non_json_bodies=False):
+        # we ignore the allow_non_json_bodies flag here because with the way invoking lambdas works,
+        # the event already is an object, so it's a moot point.
         if required and not self._event:
             raise ClientError("Request body was not valid JSON")
         return self._event
