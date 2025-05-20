8fb348871b272dc65c6729738c7c47a00f2ec540
Tom Nijboer <tom.nijboer@cimpress.com>
2025-05-20 14:22:08 +0200

    feat: add standard dependencies for cursor and backend

--- src/clearskies_aws/di/standard_dependencies.py
+++ src/clearskies_aws/di/standard_dependencies.py
@@ -1,19 +1,35 @@
 from types import ModuleType
 
 import boto3 as boto3_module
+from boto3.session import Session as Boto3Session
 from clearskies import Environment
 from clearskies.di import StandardDependencies as DefaultStandardDependencies
 
-from ..backends import DynamoDBBackend, SqsBackend
+from ..backends import (
+    DynamoDBBackend,
+    DynamoDBPartiQLBackend,
+    DynamoDBPartiQLCursor,
+    SqsBackend,
+)
 from ..secrets import ParameterStore
 
 
 class StandardDependencies(DefaultStandardDependencies):
 
+    def provide_dynamo_db_parti_ql_cursor(
+        self, boto3_session: Boto3Session
+    ) -> DynamoDBPartiQLCursor:
+        return DynamoDBPartiQLCursor(boto3_session)
+
     def provide_dynamo_db_backend(
         self, boto3: ModuleType, environment: Environment
     ) -> DynamoDBBackend:
         return DynamoDBBackend(boto3, environment)
+
+    def provide_dynamo_db_parti_ql_backend(
+        self, dynamo_db_parti_ql_cursor: DynamoDBPartiQLCursor, environment: Environment
+    ) -> DynamoDBPartiQLBackend:
+        return DynamoDBPartiQLBackend(dynamo_db_parti_ql_cursor)
 
     def provide_sqs_backend(
         self, boto3: ModuleType, environment: Environment
